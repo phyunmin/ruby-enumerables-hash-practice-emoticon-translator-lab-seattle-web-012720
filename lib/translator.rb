ca246cdb181d46cabad22b68d0f3ccb618409e25
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-18 23:12:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,14 +9,14 @@ def load_library(path)
   hash = {get_meaning:{[]}, get_emoticon:{[]}}
   data.each_pair do |key, array|
     if(hash[:get_meaning][array[1]]==nil)
-      hash[:get_meaning]={[]}
+      hash[:get_meaning]={}
       hash[:get_meaning][array[1]] = key.to_s
     else
       hash[:get_meaning][array[1]] = key.to_s
     end
     
     if(hash[:get_emoticon][array[0]]==nil)
-      hash[:get_emoticon]={[]}
+      hash[:get_emoticon]={}
       hash[:get_emoticon][array[0]] = array[1]
     else
       hash[:get_emoticon][array[0]] = array[1]
